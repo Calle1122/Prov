d8af4ce8359578ac350bbe475cae188e6325b7be
Calle <carl@lindstedt.net>
2020-10-22 15:23:30 +0200

    skapat variabler i klassen book.cs

---
+++ Prov/Book.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Prov
+{
+    public class Book
+    {
+        public int price;
+        private string name;
+        private int rarity;
+        private string category;
+        private int actualValue;
+        private bool cursed;
+        private Random generator = new Random();
+    }
+}
--- Prov/Program.cs
+++ Prov/Program.cs
@@ -6,7 +6,7 @@ namespace Prov
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
