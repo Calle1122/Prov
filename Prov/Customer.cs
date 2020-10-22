08cfbe8c17dc35f4cb1c6c7744a3ea403ac6009d
Calle <carl@lindstedt.net>
2020-10-22 15:30:51 +0200

    skapat Customer.cs klassen

---
+++ Prov/Customer.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Prov
+{
+    public class Customer
+    {
+        public string name;
+        private int age;
+        private int money;
+        private bool greedy;
+    }
+}
