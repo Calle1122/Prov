49054a299164934f7d1ef6a1dc6420fb1f97055e
Calle <carl@lindstedt.net>
2020-10-22 16:06:27 +0200

    skapat instanser av klasserna i Main

--- Prov/Program.cs
+++ Prov/Program.cs
@@ -7,7 +7,11 @@ namespace Prov
     {
         static void Main(string[] args)
         {
-            
+            //Skapar en instans av klassen Book
+            Book newItem = new Book();
+
+            //Skapar en instans av klassen Customer
+            Customer newCustomer = new Customer();
         }
     }
 }
